3f6a4c15588fd07b94070679b2b38a9b17da3269
minhchien137 <bmc13072001@gmail.com>
2026-07-15 10:05:16 +0700

    Thêm documentation tài liệu hướng dẫn sử dụng của Laser station và Print Service

--- PrintApp/obj/Debug/net8.0/PrintApp.AssemblyInfo.cs
+++ PrintApp/obj/Debug/net8.0/PrintApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7790c9cc2682a1926938d3896ff06f353cbd9843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da345d6594429d793197bbada4bdfb900843bab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
