9bbaa4dd2e5e0396edb800384cd407840eafefd8
minhchien137 <bmc13072001@gmail.com>
2026-07-22 14:54:48 +0700

    sửa lại title home

--- PrintApp/obj/Release/net8.0/win-x64/PrintApp.AssemblyInfo.cs
+++ PrintApp/obj/Release/net8.0/win-x64/PrintApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc7592fd3ab445816db27da4499056094e8150e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb0bdf5d4b815524ea8b8fe40c608d708ca1b8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
